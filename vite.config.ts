00d4048062d75eef2e2591656d4f1cba214c416a
Brian-Pob <brianpoblete77777@gmail.com>
2024-11-22 04:28:50 -0800

    try adding tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
 const ReactCompilerConfig = {
 	target: "19",
 };
@@ -10,6 +12,7 @@ export default defineConfig({
 		MillionLint.vite({
 			enabled: true,
 		}),
+		TanStackRouterVite(),
 		react({
 			babel: {
 				plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
