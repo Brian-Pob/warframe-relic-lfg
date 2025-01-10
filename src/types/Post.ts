f14ba9e5e8584536650a655ab0a28906ba930853
Brian-Pob <brianpoblete77777@gmail.com>
2025-01-10 06:55:00 -0800

    update types

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,10 +1,7 @@
-import type { Relic } from "./Relic";
-import type { User } from "./User";
-
 export type Post = {
 	post_id: string;
-	relic: Relic;
-	user_id: User;
+	relic_name: string;
+	user_id: string;
 	created_at: Date;
 	updated_at: Date;
 	open_slots: number;
